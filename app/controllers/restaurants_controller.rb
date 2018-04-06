609f8747bc39c15d0e6e6524a949a8b403cb1b35
Roberto Barros <roberto.f.barros@gmail.com>
2018-04-06 10:13:30 -0300

    rails ajax

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,17 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+  end
+
+end
